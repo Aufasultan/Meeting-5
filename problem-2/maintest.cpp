60d32c8ea9e59dba537c0f87110e54c606aa0ce2
froggy <145354889+Aufasultan@users.noreply.github.com>
2024-09-26 17:22:53 +0700

    Create maintest.cpp

---
+++ problem-2/maintest.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n = 5; //the size of vector;
+
+    vector<int> v = {
+        7, 8, 6, 4, 3
+    };
+
+    vector<int> ans; 
+    for(int i=n-1; i>=0; i--)
+    {
+        ans.push_back(v[i]);
+    }
+
+    vector<int> expectedOutput = {
+        3, 4, 6, 8, 7
+    };
+
+    if(expectedOutput==ans) cout<<"SUCCESSFULLY"<<'\n';
+    else cout<<"UNSUCCESSFULLY"<<'\n';
+    
+}
