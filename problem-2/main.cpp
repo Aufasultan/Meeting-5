6e282c03c838f7eeaaaf4f9082d471a173fdbda2
froggy <145354889+Aufasultan@users.noreply.github.com>
2024-09-26 17:00:18 +0700

    Create main.cpp

---
+++ problem-2/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int n; //the size of vector;
+    cout<<"The size of a vector: ";
+    cin>>n;
+
+    vector<int> v; 
+
+    for(int i=0; i<n; i++)
+    {
+        int x; //the value given 
+        cin>>x;
+        v.push_back(x); //push x to vector of v
+    }
+
+    vector<int> ans; //a vector contains each value of vector of v after reversing
+    for(int i=n-1; i>=0; i--)
+    {
+        ans.push_back(v[i]); 
+    }
+
+    for(auto i : ans)
+    {
+        cout<<i<<' ';
+    }
+    
+}
